b415dfe7222a681068b8c9dabd28dc4b663731cb
alena <evstafienko_a@ukr.net>
2022-12-09 21:41:11 +0200

    4

--- src/index.js
+++ src/index.js
@@ -1,35 +1,63 @@
 import { getRefs } from './js/getRefs';
 import { fetchAllPokemon, fetchPokemon} from './js/pokemonAPI';
 import { createPokemonItemsMarkup} from './js/renderFunction';
-
+import { pocemonCard, pocemonList } from './js/templates';
 
 const refs = getRefs();
 
-refs.buttonFetch.addEventListener('click', onButtonFetchClick);
-refs.pokemonList.addEventListener('click', onBtnCardClickCreateCard);
 
-console.log(refs.pokemonList);
 
-function onButtonFetchClick(evt) {
-    fetchAllPokemon().then(({ results, count }) => {
-      createPokemonItemsMarkup(results);
-  }).catch(error => console.log(error));
-};
+    fetch('https://pokeapi.co/api/v2/pokemon/charmeleon')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Упс щось пішло не так');
+        }
+        return response.json()})
+      .then((results) => {
+        console.log(results);
+        const markup = pocemonCard(results);
+          refs.pokemonList.innerHTML = markup;
+             }).catch(error => console.log(error));
 
-function onBtnCardClickCreateCard(e) {
-  let id = e.target.dataset.toggleId;
-  if (!id) return;
-  let cardPocimon = document.getElementById(id);
 
-  cardPocimon.hidden = !cardPocimon.hidden;
-//   ?
-  if( !cardPocimon.hidden){
-  let pocimonName = e.target.id;
-    refs.pokemonList.innerHTML = '';
 
-console.log(pocimonName);
+// refs.buttonFetch.addEventListener('click', onButtonFetchClick);
+// refs.pokemonList.addEventListener('click', onBtnCardClickCreateCard);
 
-}
+// console.log(refs.pokemonList);
+
+// function onButtonFetchClick(evt) {
+//     fetchAllPokemon().then(({ results, count }) => {
+//       createPokemonItemsMarkup(results);
+//   }).catch(error => console.log(error));
+// };
+
+// function onBtnCardClickCreateCard(e) {
+//   let id = e.target.dataset.toggleId;
+//   if (!id) return;
+//   let cardPocimon = document.getElementById(id);
+
+//   cardPocimon.hidden = !cardPocimon.hidden;
+// //   ?
+//   if( !cardPocimon.hidden){
+//   let pocimonName = e.target.id;
+//     refs.pokemonList.innerHTML = '';
+
+//     fetch('https://pokeapi.co/api/v2/pokemon/charmeleon')
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error('Упс щось пішло не так');
+//         }
+//         return response.json();
+//       })
+//       .then(({ results, count }) => {
+//         const markup = pocemonCard(results);
+//           refs.pocemonCard.innerHTML = markup;
+//              }).catch(error => console.log(error));
+
+// console.log(pocimonName);
+//   }
+// }
 
 
 
--- src/js/templates.js
+++ src/js/templates.js
@@ -11,12 +11,14 @@ export function pocemonCard({name, sprites, height, weight, abilities}) {
   return `
  <div >
    <div class="pocemon-wrap">
-      <img class='pocemon-img'  src="${sprites}" alt="${name}" width="80"/>
+      <img class='pocemon-img'  src="${sprites.back_default}" alt="${name}" width="80"/>
+      <img class='pocemon-img'  src="${sprites.front_default}" alt="${name}" width="80"/>
       <p class="pocemon-title">${name}</p>
     </div>
-        <p class="pocemon-text"><span class="pocemon-span">:</span>${height}</p>
-        <p class="pocemon-text"><span class="pocemon-span"></span>${weight}</p>
-        <p class="pocemon-text"><span class="pocemon-span">:</span>${abilities.name}</p>
+        <p class="pocemon-text"><span class="pocemon-span">Height:</span>${height}</p>
+        <p class="pocemon-text"><span class="pocemon-span">Weight:</span>${weight}</p>
+        <p class="pocemon-text"><span class="pocemon-span">Abilities:
+        </span>${Object.entries(...abilities)}</p>
   </div>
   `
 }
